08f5d4e5267b79cd171cf69a5268d7948e8e959b
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-01-28 14:09:47 +0530

    Initial commit

---
+++ Masai_Airline.js
@@ -0,0 +1,34 @@
+function runProgram(input) {
+    input = input.trim().split('\n')
+    let arr1 = input[1].trim().split(' ').map(Number)
+    let arr2 = input[2].trim().split(' ').map(Number)
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] <= 15 && arr2[i] <= 7) {
+            console.log('Boarding');
+        } else {
+            console.log('Stop');
+        }
+    }
+}
+if (process.env.USERNAME === "awdhesh") {
+    runProgram(`4
+     12 14 15 6
+     8 5 7 4`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
